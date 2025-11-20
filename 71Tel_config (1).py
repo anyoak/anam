06e29d3f72ca4b405f49d6a5c92e4ea324eb7c3c
anyoak <anyoakmail@gmail.com>
2025-11-20 08:25:21 +0300

    Add files via upload

---
+++ 71Tel_config (1).py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8031189126:AAFT4YpCT1mGk3LB_a6FHXut49InwAWuTAU"  # Your Telegram bot token from BotFather
+CHAT_ID = "-1002617899939"                                     # Your Telegram chat ID
+SMS_URL = "http://109.236.84.81/ints/agent/SMSCDRStats"       # URL of the SMS page to scrape
+LOGIN_URL = "http://109.236.84.81/ints/login"                  # URL of the login page
+TIMEZONE_OFFSET = 8                                             # For UTC+8 (based on your timezone)
