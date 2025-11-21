1ce3e9fa4a792359daf0091545dd5735b992149f
anyoak <anyoakmail@gmail.com>
2025-11-21 08:21:58 +0300

    Update 71Tel_config.py

--- 71Tel_config.py
+++ 71Tel_config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "8354581009:AAH28Cn3VQAdmQYb8a9q7WNEIly7x7s8moM"  # Your Telegram bot token from BotFather
-CHAT_ID = "-1002631004312"                                     # Your Telegram chat ID
-SMS_URL = "http://145.239.130.45/ints/client/SMSCDRStats"       # URL of the SMS page to scrape
-LOGIN_URL = "http://145.239.130.45/ints/login"                  # URL of the login page
+BOT_TOKEN = "8371638048:AAEHGvy-vYHmUFPXslg-2toZgOA_14osM9k"  # Your Telegram bot token from BotFather
+CHAT_ID = "-1002287664519"                                     # Your Telegram chat ID
+SMS_URL = "http://139.99.63.204/ints/agent/SMSCDRStats"       # URL of the SMS page to scrape
+LOGIN_URL = "http://139.99.63.204/ints/login"                  # URL of the login page
 TIMEZONE_OFFSET = 8                                             # For UTC+8 (based on your timezone)
